acfdfb751b82b5c7fe9d28a2800d570ca161325a
dshubhamprdxn <shubhamd@axioned.com>
2021-05-30 03:08:52 +0530

    modified: DS logic

--- helper/digitalSigning.js
+++ helper/digitalSigning.js
@@ -27,30 +27,47 @@ const encryptSign = (msg) => {
 }
 
 const verifyAndDecrypt = (enc, sig) => {
+  try {
+    // Alice has her private and Bob's public key
+    var privkeyAlice = ursa.createPrivateKey(fs.readFileSync('./alice/privkey.pem'));
+    var pubkeyBob = ursa.createPublicKey(fs.readFileSync('./bob/pubkey.pem'));
 
-  // Alice has her private and Bob's public key
-  var privkeyAlice = ursa.createPrivateKey(fs.readFileSync('./alice/privkey.pem'));
-  var pubkeyBob = ursa.createPublicKey(fs.readFileSync('./bob/pubkey.pem'));
+    let verificationFailed = false;
+    console.log('Decrypt with Alice Private; Verify with Bob Public');
+    const decyptedMessage = privkeyAlice.decrypt(enc, 'base64', 'utf8');
 
-  console.log('Decrypt with Alice Private; Verify with Bob Public');
-  const decyptedMessage = privkeyAlice.decrypt(enc, 'base64', 'utf8');
-  // if (msg !== rcv) {
-  //   throw new Error("invalid decrypt");
-  // }
+    // Converting Data to hash/buffer again
+    rcv = new Buffer.from(decyptedMessage).toString('utf8');
+    let rcvbase64 = new Buffer.from(decyptedMessage).toString('base64');
+    // rcv = new Buffer.from(decyptedMessage);
+    console.log('decyptedMessage ', decyptedMessage)
+    // console.log('rcv ', rcv)
+    // var buf = Buffer.from(decyptedMessage);
+    // console.log('buf ', buf)
 
-  // Converting Data to hash/buffer again
-  rcv = new Buffer.from(decyptedMessage).toString('base64');
-
-  if (!pubkeyBob.hashAndVerify('sha256', rcv, sig, 'base64')) {
-    // throw new Error("invalid signature");
-    console.log('Signature is Invalid. Data is Compromised');
-  } else {
-    console.log('Signature is Valid. Data is Secure');
-  }
-  // console.log('decrypted', decyptedMessage, '\n');
-  return {
-    rcv: rcv,
-    decyptedMessage: decyptedMessage
+    if (decyptedMessage !== rcv) {
+      verificationFailed = true;
+      // throw new Error("invalid decrypt");
+    }
+    if (!pubkeyBob.hashAndVerify('sha256', rcvbase64, sig, 'base64')) {
+      // throw new Error("invalid signature");
+      console.log('Signature is Invalid. Data is Compromised');
+      verificationFailed = true;
+    } else {
+      console.log('Signature is Valid. Data is Secure');
+    }
+    // console.log('decrypted', decyptedMessage, '\n');
+    return {
+      rcv: rcv,
+      decyptedMessage: decyptedMessage,
+      verificationFailed: verificationFailed
+    }
+  } catch(error) {
+    console.log('Error: ',error);
+    return {
+      decyptedMessage: '',
+      verificationFailed: true
+    }
   }
 }
 // const data = encryptSign('Hello')
